0e60b0866c7330c6c065e9fb94489a2996c6a499
gentildpinto <gentil.junior.gp@gmail.com>
2022-05-30 16:18:48 +0100

    set button click event

--- app/src/main/java/com/example/dphrases/MainActivity.java
+++ app/src/main/java/com/example/dphrases/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.dphrases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private final String[] phrases = {
+            "Frase 1",
+            "Frase 2",
+            "Frase 3",
+            "Frase 4"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button phraseBtn = findViewById(R.id.phraseBtn);
+
+        phraseBtn.setOnClickListener(v -> generatePhrase());
+    }
+
+    public void generatePhrase() {
+        TextView phraseView = findViewById(R.id.phraseView);
+
+        Random random = new Random();
+        int index = random.nextInt(this.phrases.length);
+
+        phraseView.setText(this.phrases[index]);
     }
 }
